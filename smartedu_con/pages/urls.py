9f69461a26919b32a0b2c980f0b65218e800d1b7
mialtgg <200316021@ogr.cbu.edu.tr>
2023-09-24 15:18:57 +0300

    Partials, URL - Menü, Extends - Include

---
+++ smartedu_con/pages/urls.py
@@ -0,0 +1,10 @@
+from .import views
+from django.contrib import admin
+from django.urls import path
+ 
+#path(route,view, opt(kısayol ismi))
+urlpatterns = [
+    path('',views.index,name="index"),
+   
+    
+ ]
