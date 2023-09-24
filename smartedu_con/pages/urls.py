311e5e25eba12fb7fe2e410cdb631e6fc549adff
mialtgg <200316021@ogr.cbu.edu.tr>
2023-09-24 19:59:15 +0300

    Partials, URL - Menü, Extends - Include

--- smartedu_con/pages/urls.py
+++ smartedu_con/pages/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
  
 #path(route,view, opt(kısayol ismi))
 urlpatterns = [
-    path('',views.index,name="index"),
-   
+    path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
     
- ]
+]
